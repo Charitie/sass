0b46339b1ccd12ecaef14ff0f9bc21b21abf9236
charitie <charitytarus21@gmail.com>
2020-09-27 07:51:14 +0300

    Implemented the story section

--- nexter/src/components/story/Story.js
+++ nexter/src/components/story/Story.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import './story.scss';
+import React from "react";
+import "./story.scss";
+import storyOne from "../../assets/story-1.jpeg";
+import storyTwo from "../../assets/story-2.jpeg";
 
 function Story() {
-  return (
-    <>
-    <div className='story__pictures'>
-       <h1>PICTURES</h1>
-    </div>
-    <div className='story__content'>
-        <h1>CONTENt</h1>
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div className='story__pictures'>
+				<img
+					src={storyOne}
+					alt='Couple with new house'
+					className='story__img--1'
+				/>
+				<img src={storyTwo} alt='New house' className='story__img--2' />
+			</div>
+			<div className='story__content'>
+				<h3 class='heading-3 mb-sm'>Happy Customers</h3>
+				<h2 class='heading-2 heading-2--dark mb-md'>
+					&ldquo;The best decision of our lives&rdquo;
+				</h2>
+				<p class='story__text'>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+					distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+					harum volupta!
+				</p>
+				<button class='btn'>Find your own home</button>{" "}
+			</div>
+		</>
+	);
 }
 
-export default Story
+export default Story;
