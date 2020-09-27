5deff180425f1dfe2f2d351e1ac0f88c603585d5
charitie <charitytarus21@gmail.com>
2020-09-27 07:06:21 +0300

    Implementing features section

---
+++ nexter/src/components/features/FeatureData.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ReactComponent as Globe } from "../../assets/SVG/world.svg";
+
+export const data = [
+	{
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+  },
+  {
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+  },
+  {
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+  },
+  {
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+  },
+  {
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+  },
+  {
+		image: <Globe />,
+		title: `World's best luxury homes`,
+		paragraph: `	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+    distinctio necessitatibus pariatur voluptatibus.`,
+	},
+];
