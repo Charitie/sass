96efef68e475917fba8da5f760abbf0ef35cc83a
charitie <charitytarus21@gmail.com>
2020-09-28 16:11:54 +0300

    Implementing Home section

--- nexter/src/components/homes/Home.js
+++ nexter/src/components/homes/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './Home.scss';
+import React from "react";
+import "./Home.scss";
+import { houseOne } from '../../assets/house-1.jpeg';
+
 
 const Home = () => {
-  return (
-    <div className='homes'>
-      <h1>HOME</h1>
-    </div>
-  )
-}
+	return (
+		<div className='homes'>
+			<div class='home'>
+				<div class='home'>
+					<img src={houseOne} alt='House 1' class='home__img' />{" "}
+				</div>{" "}
+      </div>
+      
+			<div class='home'>Home 2</div>
+			<div class='home'>Home 3</div>
+			<div class='home'>Home 4</div>
+			<div class='home'>Home 5</div>
+			<div class='home'>Home 6</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
