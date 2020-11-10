1840c81a035647e7540a40725f83b9e7791870af
charitie <charitytarus21@gmail.com>
2020-11-10 16:25:55 +0300

    Designing gallery section

--- nexter/src/components/gallery/Gallery.js
+++ nexter/src/components/gallery/Gallery.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Gallery.scss";
+import { galleryData } from "./galleryData";
 
 const Gallery = () => {
 	return (
 		<div className='gallery'>
-			<h1>GALLERY</h1>
+			{galleryData.map((gallery) => {
+				return (
+					<figure className={`gallery__item gallery__item--${gallery.class}`}>
+						<img
+							src={gallery.image}
+							alt='Gallery'
+							className='gallery__img'
+						/>
+					</figure>
+				);
+			})}
 		</div>
 	);
 };
---
+++ nexter/src/components/gallery/galleryData.js
@@ -0,0 +1,74 @@
+import React from "react";
+import GalleryOne from "../../assets/gal-1.jpeg";
+import GalleryTwo from "../../assets/gal-2.jpeg";
+import GalleryThree from "../../assets/gal-3.jpeg";
+import GalleryFour from "../../assets/gal-4.jpeg";
+import GalleryFive from "../../assets/gal-5.jpeg";
+import GallerySix from "../../assets/gal-6.jpeg";
+import GallerySeven from "../../assets/gal-7.jpeg";
+import GalleryEight from "../../assets/gal-8.jpeg";
+import GalleryNine from "../../assets/gal-9.jpeg";
+import GalleryTen from "../../assets/gal-10.jpeg";
+import GalleryEleven from "../../assets/gal-11.jpeg";
+import GalleryTwelve from "../../assets/gal-12.jpeg";
+import GalleryThirteen from "../../assets/gal-13.jpeg";
+import GalleryFourteen from "../../assets/gal-14.jpeg";
+
+export const galleryData = [
+  {
+    class: 1,
+		image: GalleryOne,
+  },
+  {
+    class: 2,
+		image: GalleryTwo,
+  },
+  {
+    class: 3,
+		image: GalleryThree,
+	},
+  {
+    class: 4,
+		image: GalleryFour,
+	},
+   {
+    class: 5,
+		image: GalleryFive,
+	},
+   {
+    class: 6,
+		image: GallerySix,
+	},
+   {
+    class: 7,
+		image: GallerySeven,
+	},
+   {
+    class: 8,
+		image: GalleryEight,
+	},
+   {
+    class: 9,
+		image: GalleryNine,
+	},
+   {
+    class: 10,
+		image: GalleryTen,
+	},
+   {
+    class: 11,
+		image: GalleryEleven,
+	},
+   {
+    class: 12,
+		image: GalleryTwelve,
+	},
+   {
+    class: 13,
+		image: GalleryThirteen,
+  },
+  {
+    class: 14,
+		image: GalleryFourteen,
+	},
+];
