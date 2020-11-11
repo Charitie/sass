114adcc23fd3a888f7ca751d6a5fa835ab6fccab
charitie <charitytarus21@gmail.com>
2020-11-11 14:14:54 +0300

    Designing footer

--- nexter/src/components/footer/Footer.js
+++ nexter/src/components/footer/Footer.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './Footer.scss';
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-      <h1>FOOTER</h1>
-    </div>
-  )
-}
+	return (
+		<div className='footer'>
+			<ul className='nav'>
+				<li href='#' className='nav__item nav__link'>Find your dream home</li>
+				<li href='#' className='nav__item nav__link'>Request proposal</li>
+				<li href='#' className='nav__item nav__link'>Download home planner</li>
+				<li href='#' className='nav__item nav__link'>Contact us</li>
+				<li href='#' className='nav__item nav__link'>Submit your property</li>
+				<li href='#' className='nav__item nav__link'>Find your dream home</li>
+      </ul>
+      <p className='copyright'>
+        &copy; Copyright 2020 by Charity Tarus. 
+      </p>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
